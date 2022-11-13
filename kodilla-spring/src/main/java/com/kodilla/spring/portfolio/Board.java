19f4f5752beaebb0629b25e528b8f3cbdc68ada4
Kamil Chmiel <kamil.chmiel@icloud.com>
2022-11-13 21:31:13 +0100

    Zadanie: tablica z listami zadań

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/Board.java
@@ -0,0 +1,18 @@
+package com.kodilla.spring.portfolio;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Board {
+
+    private TaskList toDoList;
+    private TaskList inProgressList;
+    private TaskList doneList;
+
+    public Board(TaskList toDoList, TaskList inProgressList, TaskList doneList) {
+        this.toDoList = toDoList;
+        this.inProgressList = inProgressList;
+        this.doneList = doneList;
+    }
+
+}
---
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -0,0 +1,37 @@
+package com.kodilla.spring.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootTest
+public class BoardTestSuite {
+
+    @Test
+    void testSet() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
+
+        boolean listToDoExist = context.containsBean("toDoList");
+        boolean listInProgressExist = context.containsBean("inProgressList");
+        boolean listDoneExist = context.containsBean("doneList");
+
+        System.out.println("List toDo exist: " + listToDoExist + " List inProgres exist: " + listInProgressExist + " List done exist: " + listDoneExist);
+    }
+
+    @Test
+    void addTest() {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.portfolio");
+        TaskList toDoList = (TaskList) context.getBean("toDoList");
+        TaskList inProgressList = (TaskList) context.getBean("inProgressList");
+        TaskList doneList = (TaskList) context.getBean("doneList");
+
+
+        toDoList.getTasks().add("newTasksListToDo");
+        System.out.println(toDoList);
+        inProgressList.getTasks().add("tasksListInProgress");
+        System.out.println(inProgressList);
+        doneList.getTasks().add("doneTasksList");
+        System.out.println(doneList);
+    }
+}
