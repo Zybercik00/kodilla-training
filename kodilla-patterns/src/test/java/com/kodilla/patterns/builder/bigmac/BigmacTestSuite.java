41c2d900377bf00ea5d4bf9591a260afb59b07f3
Kamil Chmiel <kamil.chmiel@icloud.com>
2022-11-17 18:06:32 +0100

    added assertions, variable name changed

--- kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -2,6 +2,9 @@ package com.kodilla.patterns.builder.bigmac;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BigmacTestSuite {
@@ -16,8 +19,13 @@ public class BigmacTestSuite {
                 .ingredient("onion")
                 .build();
 
-        int howManyIngredient = Bigmac.getIngredients().size();
 
-        assertEquals(2, howManyIngredient);
+        int ingredientsSize = Bigmac.getIngredients().size();
+
+        assertEquals("no sesame", Bigmac.getBun());
+        assertEquals(2, Bigmac.getBurgers());
+        assertEquals("1000 Island dressing", Bigmac.getSauce());
+        assertEquals(List.of("cucumber", "onion"), Bigmac.getIngredients());
+        assertEquals(2, ingredientsSize);
     }
 }
