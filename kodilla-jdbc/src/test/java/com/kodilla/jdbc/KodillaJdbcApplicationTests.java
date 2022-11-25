0d491d439d224ffc4d8aca34a49be2f311fe9479
Zybercik00 <109627524+Zybercik00@users.noreply.github.com>
2022-11-25 07:22:11 +0100

    kodilla-16.6 (#43)

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/KodillaJdbcApplicationTests.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/KodillaJdbcApplicationTests.java
@@ -1,13 +1,13 @@
-package kodillaspringintro;
+package com.kodilla.jdbc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class KodillaSpringIntroApplicationTests {
+class KodillaJdbcApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
