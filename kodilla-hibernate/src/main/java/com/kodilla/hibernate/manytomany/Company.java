24ef505c7ad4dda9809d3c898a9553ab41d93140
Kamil Chmiel <kamil.chmiel@icloud.com>
2022-12-02 17:54:32 +0100

    Zadanie: Wyszukujemy pracowników oraz firmy według zadanych kryteriów.

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
@@ -6,6 +6,12 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@NamedNativeQuery(
+        name = "Company.searchCompanyByFirstTreeLetters",
+        query = "SELECT * FROM Companies WHERE LEFT(company_name,3) = :NAME",
+        resultClass = Company.class
+)
+
 @Entity
 @Table(name = "COMPANIES")
 public class Company {
